64c9ab0f8479936ea6b050ee2bf2089560ce747e
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2020-12-02 09:14:57 -0300

    Formatting

--- ocaml/day1a.ml
+++ ocaml/day1a.ml
@@ -3,20 +3,15 @@ open Stdio
 
 let contains set el = Set.exists set ~f:(fun x -> x = el)
 
-let rec find_sum_pair ?set:(set=Set.empty (module Int)) input_ch =
+let rec find_sum_pair ?(set = Set.empty (module Int)) input_ch =
   let line = In_channel.input_line input_ch in
   match line with
   | None -> failwith "Not found"
-  | Some x -> 
-      let num = Int.of_string x in
-      if contains set num 
-      then num * (2020 - num)
-      else find_sum_pair ~set:(Set.add set (2020 - num)) input_ch
+  | Some x ->
+    let num = Int.of_string x in
+    if contains set num then num * (2020 - num)
+    else find_sum_pair ~set:(Set.add set (2020 - num)) input_ch
 
-let solve filename =
-  In_channel.with_file filename ~f:find_sum_pair
+let solve filename = In_channel.with_file filename ~f:find_sum_pair
 
-let () = 
-  "../data/day1a.txt"
-  |> solve
-  |> printf "%d\n";
+let () = "../data/day1a.txt" |> solve |> printf "%d\n"
